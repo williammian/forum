e3aaffa45221ad0392763cc0eb7ede08317dcff9
William Foleis Mian <william_mian@yahoo.com.br>
2020-12-06 16:48:11 -0200

    paginacao

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,7 +1,7 @@
 package br.com.alura.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,11 +10,11 @@ import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 	
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
-	List<Topico> findByCurso_Nome(String nomeCurso);
+	Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 	
 	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
-	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+	Page<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso, Pageable paginacao);
 
 }
